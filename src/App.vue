42a189bd8c032d920ca6f63aff69350d6adb2c2b
chenault <2107226736@qq.com>
2024-09-09 19:54:13 +0800

    feat(the-aside): 完成侧边栏的样式

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,11 @@
     <div class="left w-20% h-full b-A">
       <TheAside></TheAside>
     </div>
-    <div class="right w-80% h-full flex-center flex-col">
-      <span class="text-xl text-white">Hello! This is my blog</span>
-      <span class="text-xs">陈炜</span>
-      <div class="">Hello</div>
+    <div class="right w-80% h-full flex-center flex-col relative">
+      <h1 class="text-xl text-white">Hello! I'm Chenault, a full stack coder.</h1>
+      <div>
+        
+      </div>
     </div>
   </div>
 </template>
--- src/components/TheAside/TheAside.vue
+++ src/components/TheAside/TheAside.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 
+const itemList = ref([
+  { key: 1, label: 'home' },
+  { key: 2, label: 'resume'}
+])
 </script>
 
 <template>
   <ul class="the-aside-container">
-    <li class="text-tesla-gray w-full h-20 flex-center hover:cursor-pointer" v-for="item in 10" :key="item">TheAside</li>
+    <h2 class="text-white flex-center">Content</h2>
+    <li
+      class="text-tesla-gray w-full h-10 flex-center hover:cursor-pointer hover:text-white"
+      v-for="item in 10"
+      :key="item"
+    >TheAside</li>
   </ul>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
